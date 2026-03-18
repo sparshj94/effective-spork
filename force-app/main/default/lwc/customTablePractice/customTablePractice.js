ce4209e3719f216dd57666a07a4d32d704c8e5fa
Sparsh Jain <jsparsh93@gmail.com>
2026-03-18 09:18:36 +0530

    ad

--- force-app/main/default/lwc/customTablePractice/customTablePractice.js
+++ force-app/main/default/lwc/customTablePractice/customTablePractice.js
@@ -1,17 +1,16 @@
 import { LightningElement, wire } from 'lwc';
-import getAccounts from '@salesforce/apex/AccountAndContact.getAccounts'
+import getAccounts from '@salesforce/apex/AccountContactController.getAccountss'
 export default class CustomTablePractice extends LightningElement {
 
     rowLimit = 10;
     offSet=0;
     accountData = [];
-    tableCols = [];
+    tableCols = ["Name", "Industry", "Phone"];
     loadData(){
-        return getAccounts({"rowLimit":this.rowLimit, "offSet":this.offSet}).then(data=>{
+        return getAccounts({"limitSize":this.rowLimit, "offSet":this.offSet}).then(data=>{
             this.accountData = data;
-            this.tableCols = [...data[0].Name];
-            this.tableCols = [...data[0].Phone];
-            this.tableCols = [...data[0].Industry];
+            console.log(data);
+            
         }).catch(err=>{
             console.log(err);
         })
